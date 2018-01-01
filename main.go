07a6d158175bb762d8fb88ce82a0fdb140607722
Nicholas Wu <nickwu241@gmail.com>
2017-12-31 16:44:38 -0800

    Use PORT from env if set

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-const port = 8080
+import (
+	"os"
+	"strconv"
+)
+
+const defaultPort = 8080
 
 func main() {
-	s := NewServer(port)
+	var s Server
+	// Use PORT from environment variables if it's set.
+	if portEnv := os.Getenv("PORT"); portEnv != "" {
+		port, err := strconv.Atoi(portEnv)
+		if err != nil {
+			panic("can't convert PORT env into integer")
+		}
+		s = NewServer(port)
+	} else {
+		s = NewServer(defaultPort)
+	}
 	s.Start()
 }
